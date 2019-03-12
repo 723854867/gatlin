2d0bd388d8cf5b4b344974106cae00ee8fb39cd9
樊水东 <531273925@qq.com>
2019-03-12 11:42:42 +0800

    fsd

--- gatlin-soa/gatlin-soa-account/gatlin-soa-account-service/src/main/java/org/gatlin/soa/account/mybatis/dao/AccountDao.java
+++ gatlin-soa/gatlin-soa-account/gatlin-soa-account-service/src/main/java/org/gatlin/soa/account/mybatis/dao/AccountDao.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.gatlin.dao.mybatis.DBDao;
 import org.gatlin.soa.account.bean.entity.Account;
 
@@ -12,5 +13,8 @@ public interface AccountDao extends DBDao<Long, Account> {
 
 	@MapKey("owner")
 	Map<Long, Account> getByUserIds(@Param("userIds") Set<Long> userIds);
+
+	@Select("SELECT * FROM account WHERE TYPE = 1 AND owner_type = 1 AND OWNER = #{userId}")
+	Account getByUserId(@Param("userId")long userId);
 
 }
