0eb6bc280888ad76a15312e9f3b2d5095fece03a
樊水东 <531273925@qq.com>
2019-04-08 13:43:46 +0800

    fsd

--- gatlin-sdk/gatlin-sdk-heepay/src/main/java/org/gatlin/sdk/heepay/HeepayConfig.java
+++ gatlin-sdk/gatlin-sdk-heepay/src/main/java/org/gatlin/sdk/heepay/HeepayConfig.java
@@ -3,8 +3,8 @@ package org.gatlin.sdk.heepay;
 public interface HeepayConfig {
 
 	//小额正式交易地址
-//	String SMALL_AMOUNT_URL = "https://Pay.heepay.com/API/PayTransit/PayTransferWithSmallAll.aspx";
-	String SMALL_AMOUNT_URL = "http://211.103.157.45/payheepay/API/PayTransit/PayTransferWithSmallAll.aspx";
+	String SMALL_AMOUNT_URL = "https://Pay.heepay.com/API/PayTransit/PayTransferWithSmallAll.aspx";
+//	String SMALL_AMOUNT_URL = "http://211.103.157.45/payheepay/API/PayTransit/PayTransferWithSmallAll.aspx";
 	String SMALL_AMOUNT_HOST = "211.103.157.45";
 	int SMALL_AMOUNT_PORT = 80;
 	String SMALL_AMOUNT_PATH = "/payheepay/API/PayTransit/PayTransferWithSmallAll.aspx";
@@ -13,7 +13,7 @@ public interface HeepayConfig {
 	//商户id
 	String agent_id = "2119474";
 	//商户付款理由
-	String remit_reason = "上游厂商结算款";
+	String remit_reason = "微钱进兑付";
 	//批量付款回调地址
 	String notify_url = "";
 	//商户自定义原样返回，最大长度50个字符
